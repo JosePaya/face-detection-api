7a4a804d66272edb6f2ee3e74984a4cca7cc0878
JosePaya <jmpayaalvarez@gmail.com>
2019-08-23 22:50:50 +0100

    Adding PORT and pushing nodemon to prod dependencies

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt
 app.put('/image', (req, res) => image.handleImage(req, res, db))
 app.post('/imageurl', (req, res) => image.handleClarifaiCall(req, res))
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`)
 })
