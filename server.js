95d522270283405192b09e2d3bff5649c772e015
Jose Paya P <MPM-JPaya-P@mitie.com>
2019-08-26 22:00:55 +0100

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image')
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'project-face-detection'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
